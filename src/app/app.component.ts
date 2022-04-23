5edee2401ea20ac647e01048f733f34ab43dbccf
l3miage-zodjihok <hamegan252@gmail.com>
2022-04-23 16:14:41 +0200

    affichages listes des pokemons dans la template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import {POKEMONS} from "./mock-pokemons";
+import {Pokemon} from "./pokemon";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
   title = 'pokemons';
+  pokemonList:Pokemon[]=POKEMONS;
+  pokemonSlected: Pokemon|undefined ;
+
+  ngOnInit() {
+    console.log(this.pokemonList);
+
+
+  }
+  selectPokemon(pokemonId :string){
+    const pokemon:Pokemon|undefined =this.pokemonList.find(pokemon=>pokemon.id ==+pokemonId)
+    if (pokemon){
+      console.log(`vous avez selectionner le pokemon ${pokemon.name}`)
+      this.pokemonSlected=pokemon
+    }
+    else {
+      console.log(`le pokemon selectionner n'hesiste pas`)
+      this.pokemonSlected=pokemon
+    }
+
+  }
 }
